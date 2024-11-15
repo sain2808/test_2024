417cc616b6f3d9182c77f5bfd1156f40f390b2b2
sain2808 <hoangdat753@gmail.com>
2024-11-16 00:35:39 +0700

    hieu

--- Source (1).cpp
+++ Source (1).cpp
@@ -2,12 +2,23 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+return a-b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int a,b;
+	cout << "Nhap a";
+	cin>>a;
+	cout << "Nhap b";
+	cin>>b;
+	cout << "Hieu la" << hieu(a,b)<<endl;
+	
+
 
 	system("pause");
 	return 0;
